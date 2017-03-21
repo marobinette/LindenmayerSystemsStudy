dc57c039f624b9c765151599838601dfe633f691
marobinette <michael.robinette@careerbuilder.com>
2017-03-21 16:10:47 -0400

    Driver script creates square-spikes fractal

---
+++ LsystemDriver.m
@@ -0,0 +1,6 @@
+ % Lsystem(numberOfIterations,seed,rules,delta,len)
+ % original code at http://courses.cit.cornell.edu/bionb441/LSystem/index.html
+ % modified by Michael Robinette 2/1/2017
+ title = 'Square-Spikes by Michael Robinette'
+ rulesForSpike = {'F','F17-F34+F17-F'};
+ Lsystem(5,'F18-F18-F18-F',rulesForSpike,5,.7,title);
